31959a70e981a7b704bc236071e3ab2da01913a5
L0ny1ucif32 <darshanputtaswamy@gmail.com>
2023-08-15 05:49:22 +0530

    activities repo

---
+++ lib/domain/entity/activities/activities_list.dart
@@ -0,0 +1,18 @@
+import 'package:boilerplate/domain/entity/activities/activities.dart';
+
+class StoreActivitiesList {
+  final List<StoreActivities>? activities;
+
+  StoreActivitiesList({
+    this.activities,
+  });
+
+  factory StoreActivitiesList.fromJson(List<dynamic> json) {
+    List<StoreActivities> activities = <StoreActivities>[];
+    activities = json.map((activities) => StoreActivities.fromMap(activities)).toList();
+
+    return StoreActivitiesList(
+      activities: activities,
+    );
+  }
+}
---
+++ lib/domain/repository/activities/activities_repository.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:boilerplate/domain/entity/activities/activities.dart';
+import 'package:boilerplate/domain/entity/activities/activities_list.dart';
+
+abstract class PostRepository {
+  Future<StoreActivitiesList> getStoreActivitiesList(String store_uid);
+
+  Future<List<StoreActivitiesList>> findStoreActivityById(String uid);
+
+  Future<int> insert(StoreActivities activities);
+
+  Future<int> update(StoreActivities activities);
+
+  Future<int> delete(StoreActivities activities);
+}
